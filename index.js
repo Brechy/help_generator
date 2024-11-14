ca0e2c1f13195b12b4ce7233b3d8f2afec74e221
Brechin Andariese <brechin@brechy.net>
2024-11-14 15:22:37 -0600

    Initial commit.
    Add chores object to index.js and setup package.json

---
+++ index.js
@@ -0,0 +1,52 @@
+const chores = [{
+    day: "Monday",
+    chore: "sweep and mop kitchen floor"
+},
+{
+    day: "Monday",
+    chore: "sweep and mop livingroom floor"
+},
+{
+    day: "Tuesday",
+    chore: "scrub toilets"
+},
+{
+    day: "Tuesday",
+    chore: "clean bathroom sinks"
+},
+{
+    day: "Wedneday",
+    chore: "wipe down kitchen cabinets"
+},
+{
+    day: "Wedneday",
+    chore: "wipe all windows"
+},
+{
+    day: "Thursday",
+    chore: "Empty all trashcans around the house."
+},
+{
+    day: "Thursday",
+    chore: "Throw out any clothing that is damaged, doesn't fit, or that you have not work in 3-6 months."
+},
+{
+    day: "Thursday",
+    chore: "Shred and throw out all junk mail, catalogs, magazines, etc."
+},
+{
+    day: "Thursday",
+    chore: "Discard school or craft projects you are not keeping."
+},
+{
+    day: "Friday",
+    chore: "wash, dry, and put away all personal laundry"
+},
+{
+    day: "Friday",
+    chore: "wash all bedding"
+},
+{
+    day: "Saturday",
+    chore: "set timer for 20 minutes and clean what you can see"
+}]
